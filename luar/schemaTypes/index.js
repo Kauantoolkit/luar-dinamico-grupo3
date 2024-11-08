dba7bb9812e6cf3632679dc44c14b3de12a12e72
Kauan Cristian <107585090+Kauantoolkit@users.noreply.github.com>
2024-11-08 19:30:22 -0300

    a

--- luar/schemaTypes/index.js
+++ luar/schemaTypes/index.js
@@ -1,7 +1,10 @@
 
 
+import alguns_nossos_produtos from './alguns_nossos_produtos'
 import linkinsta from './linkinsta'
 import linkwhats from './linkwhats'
+import mais_vendidos from './mais_vendidos'
+import nova_linha from './nova_linha'
 import person from './person'
 
-export const schemaTypes = [person, linkwhats, linkinsta]
+export const schemaTypes = [person, linkwhats, linkinsta, alguns_nossos_produtos, mais_vendidos, nova_linha]
